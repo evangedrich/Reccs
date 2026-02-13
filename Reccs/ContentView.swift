c8ddb063ffce628749e14eb17709123b3ed48ce7
Evan Gedrich <116127782+evangedrich@users.noreply.github.com>
2026-02-13 13:27:02 -0500

    Initial Commit

---
+++ Reccs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reccs
+//
+//  Created by Evan Gedrich Pintado on 2/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reccs/ReccsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReccsApp.swift
+//  Reccs
+//
+//  Created by Evan Gedrich Pintado on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ReccsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
